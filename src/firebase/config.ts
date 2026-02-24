c8835ca174407dcd3969f3975841e2f448707418
Aljssary Abdullah <aljssary2017@gmail.com>
2026-02-24 15:50:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5329262863-6220e",
+  "appId": "1:698169743937:web:d2f0ef97aa4087deaec4c0",
+  "apiKey": "AIzaSyAePKxwmzatDvEsHGlgCzCCoh0uPS-mszI",
+  "authDomain": "studio-5329262863-6220e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "698169743937"
+};
